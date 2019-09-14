9856ffb61391557b4b4e3eac672ad58b1b788ca3
piak12322 <newverchirr@gmail.com>
2019-09-15 00:59:38 +0300

    add api

---
+++ app/Http/Controllers/Auth/Api/ApiController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Api;
+
+use App\Http\Requests\UserRequest;
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    protected $users;
+    protected $user;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct(User $users)
+    {
+        $this->users = $users;
+       // $this->user = Auth::user();
+
+    }
+
+    public function index()
+    {
+        return ['token'=>Auth::user()->api_token];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = Hash::make($data['password']);
+        return User::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->users->findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
